29f02e5012ca368490174d7988c894e38cd803f7
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2021-06-15 20:24:09 +0300

    first push

---
+++ src/App.Styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const MainContainer = styled("main")`
+  max-width: 1920px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  background: #ddd;
+  flex-direction: column;
+
+  @media screen and (min-width: 1920px) {
+    margin: 0 auto;
+  }
+`;
+
+export const Typography = styled("p")`
+  font-size: ${(props) => props.fontSize}px;
+  color: ${(props) => props.color};
+`;
+
+export const FlexBox=styled('div')`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: auto;
+    background: ${(props) => props.color};
+`;
+
+export const BackgroundedFlexBox=styled(FlexBox)`
+    background: #000;
+    `
+
+export const FlexRow=styled(FlexBox)`
+  flex-direction: row;
+`;
+
+export const FlexColumn=styled(FlexBox)`
+  flex-direction: column;
+`;
+
+export const InnerSection=styled(FlexColumn)`
+  max-width: 1640px;
+  width:100%;
+  margin: 0 auto;
+`;
