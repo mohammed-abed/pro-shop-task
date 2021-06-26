97644ae8fd8f3dddb1d3a654546546b81dd892b6
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2021-06-26 07:58:23 +0300

    Task 5 added missing files

---
+++ src/Redux/User/userReducer.js
@@ -0,0 +1,119 @@
+import {
+  USER_LOGIN_FAILED,
+  USER_LOGIN_START,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT_SUCCESS,
+  USER_REGISTER_FAILED,
+  USER_REGISTER_START,
+  USER_REGISTER_SUCCESS,
+} from "./userTypesConstants";
+
+/*
+export const userReducer = (
+  initialState = {
+    user: {
+      _id: "",
+      name: "",
+      email: "",
+      token: "",
+    },
+  },
+  action
+) => {
+  /!**
+     * action={
+            type:String,
+            payload:
+     * }
+     * *!/
+
+  switch (action.type) {
+    case USER_LOGIN_SUCCESS:
+      return {
+        user: action.payload,
+      };
+
+    default:
+      return initialState;
+  }
+};
+*/
+export const userReducer = (
+  initialState = {
+    user: {
+      _id: "",
+      name: "",
+      email: "",
+      token: "",
+    },
+    success: false,
+    isLoading: false,
+    error: "",
+  },
+  action
+) => {
+  /**
+   action={
+            type:String,
+            payload:
+      }
+   * */
+
+  switch (action.type) {
+    /** LOGIN cases */
+    case USER_LOGIN_START:
+      return {
+        ...initialState,
+        isLoading: true,
+      };
+
+    case USER_LOGIN_SUCCESS:
+      return {
+        user: action.payload,
+        isLoading: false,
+        success: true,
+      };
+
+    case USER_LOGIN_FAILED:
+      return {
+        ...initialState,
+        error: action.payload,
+        isLoading: false,
+      };
+
+    /** Register cases */
+    case USER_REGISTER_START:
+      return {
+        ...initialState,
+        isLoading: true,
+      };
+
+    case USER_REGISTER_SUCCESS:
+      return {
+        user: action.payload,
+        isLoading: false,
+        success: true,
+      };
+
+    case USER_REGISTER_FAILED:
+      return {
+        ...initialState,
+        error: action.payload,
+        isLoading: false,
+      };
+
+    /** Logout cases */
+    case USER_LOGOUT_SUCCESS:
+      return {
+        user: {
+          _id: "",
+          name: "",
+          email: "",
+          token: "",
+        },
+      };
+
+    default:
+      return initialState;
+  }
+};
