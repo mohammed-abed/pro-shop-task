c0f4f84fdad20d464a7beb746d6c7ff2b0bd861c
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2021-07-14 02:41:07 +0300

    Update on Payment,Optimize

--- src/Redux/Order/orderActions.js
+++ src/Redux/Order/orderActions.js
@@ -1,15 +1,6 @@
 import { PLACE_ORDER_START } from "./orderTypes";
 import axios from "axios";
 
-export const placeOrder = () => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: PLACE_ORDER_START,
-    });
-
-    const { data } = axios.post("");
-  } catch (e) {}
-};
 export const placeOrder = (history) => async (dispatch, getState) => {
   try {
     dispatch({
